c3dab6627ea8191fe0b46fdfbf8f075d1434508b
Mario Slavov <hellsflash@gmail.com>
2017-08-06 05:56:55 +0300

    Add project files.

---
+++ Skeleton.Tests/AxeTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class AxeTests
+    {
+        private const int AxeAttack = 1;
+        private const int AxeDurability = 1;
+        private const int DummyHealth = 20;
+        private const int DummyXP = 1;
+
+        private Axe axe;
+        private Dummy dummy;
+
+        [SetUp]
+        public void TestInit()
+        {
+            this.axe = new Axe(AxeAttack, AxeDurability);
+            this.dummy = new Dummy(DummyHealth, DummyXP);
+        }
+
+        [Test]
+        public void AxeLosesDurabilityAfterAtack()
+        {
+            axe.Attack(dummy);
+
+            Assert.AreEqual(0, axe.DurabilityPoints, "Axe Durability doesn't change after attack");
+        }
+
+        [Test]
+        public void AttackingWithBrokenAxe()
+        {
+
+            axe.Attack(dummy);
+
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+        }
+    }
+}
---
+++ Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        private const int AxeAttack = 1;
+        private const int AxeDurability = 1;
+        private const int DummyHealth = 1;
+        private const int SecondDummyHealth = 1;
+        private const int DummyXP = 1;
+
+        private Axe axe;
+        private Dummy dummy;
+        private Hero hero;
+        private Dummy secondDummy;
+
+        [SetUp]
+        public void TestInit()
+        {
+            this.axe = new Axe(AxeAttack, AxeDurability);
+            this.dummy = new Dummy(DummyHealth, DummyXP);
+            this.hero = new Hero("Mario");
+            this.secondDummy = new Dummy(SecondDummyHealth, DummyXP);
+        }
+
+        [Test]
+        public void DummyLosesHealthIfAttacked()
+        {
+            axe.Attack(dummy);
+
+            Assert.AreEqual(0, dummy.Health, "Dummy dosen't lose hp");
+        }
+
+        [Test]
+        public void CanAttackDeadDummy()
+        {
+            axe.Attack(dummy);
+
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+        }
+
+        [Test]
+        public void DeadDummyCanGiveXP()
+        {
+            hero.Attack(dummy);
+
+            Assert.AreEqual(1, hero.Experience, "Dead Dummy doesn't give XP");
+        }
+
+        [Test]
+        public void AliveDummyCantGivesXP()
+        {
+            hero.Attack(secondDummy);
+
+            Assert.Throws<InvalidOperationException>(() => hero.Attack(secondDummy));
+        }
+    }
+}
